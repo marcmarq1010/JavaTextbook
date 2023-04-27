1a241a660d247232a25a5a36ff7df120731232b8
Marcos <mmarquez144@toromail.csudh.edu>
2023-04-27 07:30:34 -0700

    m

--- JavaChapter6ProgrammingChallenges/Problem11/FreezingAndBoilingPoints.java
+++ JavaChapter6ProgrammingChallenges/Problem11/FreezingAndBoilingPoints.java
@@ -1,5 +1,66 @@
 
 public class FreezingAndBoilingPoints 
 {
+	private double temp;
+	private boolean ethylFreezingPoint;
+	private boolean ethylBoilingPoint;
+	private boolean oxygenlFreezingPoint;
+	private boolean oxygenlBoilingPoint;
+	private boolean waterlFreezingPoint;
+	private boolean waterBoilingPoint;
 	
+	public FreezingAndBoilingPoints(double temp)
+	{
+		this.temp = temp;
+	}
+	
+	public FreezingAndBoilingPoints()
+	{
+		
+	}
+
+	public double getTemp() 
+	{
+		return temp;
+	}
+	
+	public void setTemp(double temp)
+	{
+		this.temp = temp;
+	}
+
+	public boolean isEthylFreezing() 
+	{
+		return ethylFreezingPoint = temp <= -173?true:false;
+	}
+
+	public boolean isEthylBoiling() 
+	{
+		//172
+		return ethylBoilingPoint = temp >= 172?true:false;
+	}
+
+	public boolean isOxygenlFreezing() 
+	{
+		//-362
+		return oxygenlFreezingPoint = temp <=-362?true:false;
+	}
+
+	public boolean isOxygenlBoiling() 
+	{
+		//-306
+		return oxygenlBoilingPoint = temp >= -306?true:false;
+	}
+
+	public boolean isWaterlFreezing()
+	{
+		//32
+		return waterlFreezingPoint = temp <= 32?true:false;
+	}
+
+	public boolean isWaterBoiling() 
+	{
+		//212
+		return waterBoilingPoint = temp >= 212?true:false;
+	}
 }
--- JavaChapter6ProgrammingChallenges/Problem11/FreezingAndBoilingPointsTestClass.java
+++ JavaChapter6ProgrammingChallenges/Problem11/FreezingAndBoilingPointsTestClass.java
@@ -1,4 +1,13 @@
 
-public class FreezingAndBoilingPointsTestClass {
-
+public class FreezingAndBoilingPointsTestClass 
+{
+	public static void main(String[] args)
+	{
+		
+	}
+	
+	public static FreezingAndBoilingPoints createClass()
+	{
+		return new FreezingAndBoilingPoints();
+	}
 }
